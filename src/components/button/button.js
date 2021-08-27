f496cdd06c8d0dce8c1a8f60e2ee41237a14c016
Kirill.Alekseev <kirill.alekseev@x5.ru>
2021-08-27 12:54:00 +0300

    changed buttom component

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,11 +1,11 @@
 ﻿import React from 'react';
 import './button.css';
 
-const button = ({text, onClick, className, icon}) => {
+const button = ({text, onClick, className, icon = ''}) => {
   return (
     <button className={`button ${className}`} onClick={onClick}>
       <span className="button__text">
-        {icon ? icon : ''}
+        {icon}
         {text}
       </span>
     </button>
